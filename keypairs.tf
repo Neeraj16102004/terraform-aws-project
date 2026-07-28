de8a79164b29a7d5f32dd17367c1f90a2234cc33
Neeraj Rana <neerajrana@Neerajs-MacBook-Air.local>
2026-07-28 19:28:25 +0530

    Initial commit (excluded binary providers)

---
+++ keypairs.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "vprofilekey" {
+  key_name   = "vprofilekey"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBE/h/uNhcOSUeYj/U2BL4tyOrHIKyZHm/j+oErgi65k neerajrana@Neerajs-MacBook-Air.local"
+}
